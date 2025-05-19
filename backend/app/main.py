1354802a83f5934a09517591121ad26dec889556
Abhishek Kumar <abhik12295@gmail.com>
2025-05-19 14:15:50 -0500

    Added Contact Page

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from backend.app.routes import auth, dashboard
+from backend.app.routes import auth, dashboard, contact
 from backend.app.auth_config import auth_middleware
 from pathlib import Path
 from backend.app.auth_config import get_current_user
@@ -31,9 +31,14 @@ templates = Jinja2Templates(directory=BASE_DIR / "frontend/templates")
 # Include Routes
 app.include_router(auth.router)
 app.include_router(dashboard.router,prefix="/dashboard")
+#app.include_router(contact.router,prefix="/contact")
 # app.include_router(dashboard_router)
 
 # Serve the index page
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/contact", response_class=HTMLResponse)
+def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
